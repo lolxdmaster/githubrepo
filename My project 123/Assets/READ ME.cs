35806666a1707bdb24e3f52873d56bb26a828328
lolxdmaster <ronaldortiz5019@gmail.com>
2025-04-24 20:24:22 -0400

    sex
    sex

---
+++ My project 123/Assets/READ ME.cs
@@ -0,0 +1,70 @@
+/* okay so in this project, me n u arent gonna be making games on here but just fuck around and practice the locations and scripting
+   aswell as using the coding knowledge you will have into such aspects of developing in unity
+  
+------------------------------------------------------------------------------------------------------- 
+best way to memorize the functions is what ur gonna add here to remember like for example
+transform - the location of the position, scale and rotation of the sprite
+
+transform.translate(xyz) - the movement of a sprite
+
+void update - like an loop
+void start - happens the moment the game starts
+
+something like that
+--------------------------------------------------------------------------------------------------------
+anyways here some parts tha id highly recommend you learning even outside of unity like in use of a console app
+
+1. printing
+2. variables like int, double and float
+3. If statements
+4, switches like booleon (true and false)
+5. making functions
+
+unity stuff id have learn if i can remember
+
+1. the movement of an player using if statements
+
+2. the transform location like making an teleportation, rotating and movement if an key is pressed
+
+4. an movement of an object without using any keys, maybe an moving platform or a spinning object
+
+5. collision detection in use of gameobject.collision or void oncollisionenter2d
+
+6. collsion with use of triggers (void ontriggerenter2d)
+
+7. an integer changing each action is made like for example, when the key F is pressed, int x will be added up by 1 but key G will decrease the number by 1
+
+8. find any locations on the INSPECTOR that can be use as certain stuff to make like camera sizes and gameobject colors
+
+9. UI and Buttons, same as 6, find an location that can be used as stuffs like the ui disabling and enabling
+
+10. Buttons that have an function and will cause an action if pressed
+
+11. audio scripting
+
+12. raycasting
+
+
+(before starting off scripting, id suggest fooling around with objects like creating an sprite, coloring and changing its appearances or maybe changing the size and location of the camera)
+
+                                                  NOTES
+--------------------------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
